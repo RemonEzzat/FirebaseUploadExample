3a844d07b540b592f6fef291b8ea5ea10040eb2c
Your Name <aabdelsayad14@gmail.com>
2018-08-28 10:07:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/remon/firebaseuploadexample/Upload.java
@@ -0,0 +1,44 @@
+package com.example.remon.firebaseuploadexample;
+
+import com.google.firebase.database.Exclude;
+
+public class Upload {
+    private String mName;
+    private String mImageUrl;
+    private String mKey;
+    public Upload(){
+
+    }
+
+    public Upload(String mName , String mImageUrl) {
+        if(mName.trim().equals("")){
+            mName = "No Name";
+        }
+        this.mName = mName;
+        this.mImageUrl = mImageUrl;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setmImageUrl(String mImageUrl) {
+        this.mImageUrl = mImageUrl;
+    }
+    @Exclude
+    public String getmKey() {
+        return mKey;
+    }
+    @Exclude
+    public void setmKey(String mKey) {
+        this.mKey = mKey;
+    }
+}
